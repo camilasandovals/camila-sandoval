a1ff4414156b903809fd92a6b9ea4b94dba69db7
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-04-19 21:43:12 -0400

    added first files for the portfolio

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import { Col, Container, Row } from "react-bootstrap";
+
+export default function Header () {
+    return(
+        <header>
+            <Container>
+                <Row>
+                    <Col>
+                        <h1> Hello, I am Camila</h1>
+                    </Col>
+                </Row>
+            </Container>
+        </header>
+    )
+}
